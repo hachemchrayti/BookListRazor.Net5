84c536a47397bd762cf7bdf2fe71e420a0a7eb5f
hachemchrayti <hachemfst@gmail.Com>
2021-03-26 20:26:46 +0100

    Add database connection and Book table using migrations

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
