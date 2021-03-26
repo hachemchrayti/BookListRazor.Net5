cec7aa09e0570af0392bb3df8384020a6d3f9cfb
hachemchrayti <hachemfst@gmail.Com>
2021-03-26 22:20:24 +0100

    CRUD operations with the use of Boostrap datatable and Web Api call to delete a book

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -14,7 +14,7 @@ namespace BookListRazor.Model
 
         public string Author { get; set; }
 
-
+        public string ISBN { get; set; }
 
     }
 }
--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -25,6 +25,7 @@ namespace BookListRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ApplicationDbConnection")));
             services.AddRazorPages();
         }
@@ -52,6 +53,7 @@ namespace BookListRazor
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
